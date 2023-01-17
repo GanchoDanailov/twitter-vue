e5b81bab8b32f00d5ccf2d16ad17d840d35541a6
Gancho Danailov <Gancho.danailov@gmail.com>
2023-01-17 12:17:45 +0200

    init project

---
+++ src/layouts/default/Default.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer">
+      <v-card class="mx-auto" max-width="300">
+        <v-list>
+
+          <v-list-item v-for="(item, i) in items" :key="i" :value="item" color="primary" variant="plain">
+            <template v-slot:prepend>
+              <v-icon :icon="item.icon"></v-icon>
+            </template>
+
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Tvvitter</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+
+
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: null,
+    items: [
+      { text: 'Home', icon: 'mdi-account' },
+      { text: 'Contact', icon: 'mdi-flag' },
+    ],
+  }),
+}
+</script>
