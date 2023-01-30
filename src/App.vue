4584944f8fd04bbeb65b1b74e46ff217c3d94b18
Gancho Danailov <gancho.danailov@pedalsure.com>
2023-01-30 23:33:37 +0200

    refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,35 @@
 <template>
-  <router-view />
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" class="pl-8">
+      <v-btn
+        class="ma-2"
+        variant="text"
+        icon="mdi-twitter"
+        size="x-large"
+        color="blue-lighten-2"
+      />
+
+      <NavigationMenu />
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-toolbar-title @click="drawer = !drawer">Twitter</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-<script setup>
-//
+
+<script>
+import NavigationMenu from "@/components/NavigationMenu";
+
+export default {
+  data: () => ({
+    drawer: null,
+  }),
+  components: { NavigationMenu },
+};
 </script>
---
+++ src/components/NavigationMenu.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="d-flex flex-column">
+    <router-link to="/" class="d-flex align-center text-black mb-3">
+      <v-icon size="x-large" icon="mdi-home-heart"></v-icon>
+      <span class="ml-5 text-h6">Home</span>
+    </router-link>
+
+    <!-- <router-link to="/about" class="d-flex align-center text-black mb-3">
+          <v-icon size="x-large" icon="mdi-pound"></v-icon>
+          <span class="ml-5 text-h6">Explore</span>
+        </router-link> -->
+
+    <router-link to="/login" class="d-flex align-center text-black mb-3">
+      <v-icon size="x-large" icon="mdi-login-variant"></v-icon>
+      <span class="ml-5 text-h6">Login</span>
+    </router-link>
+
+    <router-link to="/register" class="d-flex align-center text-black mb-3">
+      <v-icon size="x-large" icon="mdi-account-plus-outline"></v-icon>
+      <span class="ml-5 text-h6">Register</span>
+    </router-link>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+a {
+  text-decoration: none;
+}
+.router-link-exact-active {
+  span {
+    font-weight: 900;
+  }
+}
+</style>
